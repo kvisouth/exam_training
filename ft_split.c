f8d8882a38e766db312a3d0f64ed06bd83659b85
nokosse <visouthivong.kevin@gmail.com>
2023-01-06 20:48:18 +0100

    SUCCESS

--- ft_split.c
+++ ft_split.c
@@ -1,77 +1,63 @@
 #include <stdlib.h>
+#include <stdio.h>
 
-//Verification si lettre est un separateur
-int is_sep(char letter)
+int is_sep(char c)
 {
-    if (letter == ' ' || letter == '\t' || letter == '\n')
+    if (c == ' ' || c == '\n' || c == '\t')
         return (1);
-    return (0);
+    return(0);
 }
 
-//Retourne la longueur d'un mot (pr malloc dans strdup)
 int wdlen(char *str)
 {
     int i = 0;
     while (str[i])
     {
         if (is_sep(str[i]) == 1)
-            return (i);
+            return(i);
         i++;
     }
-    return (i);
+    return(i);
 }
 
-//Retourne le nombre de mots dans str (pr malloc dans split)
 int tablen(char *str)
 {
     int i = 0;
-    int cpt = 0;
-    
+    int c = 0;
     while (str[i])
     {
-        while(is_sep(str[i] == 1 && str[i]))
+        while (is_sep(str[i]) == 1 && str[i])
             i++;
         if (str[i])
-        {
-            cpt++;
-            i++;
-        }
-        while (is_sep(str[i] == 0) && str[i])
+            c++;
+        while (is_sep(str[i]) == 0 && str[i])
             i++;
     }
-    return (cpt);
+    return(c);
 }
 
-//strdup de 0 a sep
 char *word(char *str)
 {
     int len = wdlen(str);
     int i = 0;
-    char *new = malloc(len + 1);
+    char *new = malloc (len + 1);
     if (!new)
-        return(NULL);
-    i = 0;
+        return (NULL);
     while (i < len)
     {
         new[i] = str[i];
         i++;
     }
     new[i] = '\0';
-    return (new);
+    return(new);
 }
 
-
-//1. malloc la taille du tab de tab
-//2. parcours *str jusqua \0
-//3. dans while, check si on est sur sep ou \0
-//4. on est sur un mot, strndup ce mot dans le 1er **tab & passe au prochain **tab
-//5. vu quon a strndup notre mot, osef du mot dont incremente str jusaqua un sep ou un 0
-char    **ft_split(char *str)
+char **ft_split(char *str)
 {
-    char **split = malloc((sizeof(char *) * (tablen(str) + 1)));
-    if (!split)
-        return (NULL);
     int i = 0;
+    char **split = malloc((tablen(str) + 1) * (sizeof(char *)));
+    if (!split)
+        return(NULL);
     while (*str)
     {
         while (is_sep(*str) == 1 && *str)
@@ -85,20 +71,19 @@ char    **ft_split(char *str)
             str++;
     }
     split[i] = NULL;
-    return (split);
+    return(split);
 }
 
-// #include <stdio.h>
-// int main (int ac, char **av)
-// {
-//     int i = 0;
-//     if (ac == 2)
-//     {
-//         char **split = ft_split(av[1]);
-//         while (split[i])
-//         {
-//             printf("%s\n",split[i]);
-//             i++;
-//         }
-//     }
-// }
+int main (int ac, char **av)
+{
+    if (ac == 2)
+    {
+        int i = 0;
+        char **split = ft_split(av[1]);
+        while (split[i])
+        {
+            printf("%s\n",split[i]);
+            i++;
+        }
+    }
+}
