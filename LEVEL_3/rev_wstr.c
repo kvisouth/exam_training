b4e82c1a9ff4cb37383029060353d925129a0101
nokosse <visouthivong.kevin@gmail.com>
2023-01-10 20:21:10 +0100

    edited comments

--- LEVEL_3/rev_wstr.c
+++ LEVEL_3/rev_wstr.c
@@ -9,44 +9,45 @@ int main(int ac, char **av)
 	int start;
 	int end;
 	int i = 0;
-	int flag = 0;
+	int flg = 0;
 		
 	if(ac == 2)
 	{   
 		char *str = av[1];
 
-		//On se cale sur la fin de la string.
+		// 1. On cale i sur la fin de la string.
 		while(str[i] != '\0')
 			i++;
+		
+		// 2. Boucle de la fin, jusqu'au debut pour str[i]
 		while(i >= 0)
 		{
-			//On recule i tant que str[i] n'est pas une lettre
+			// 3. On recule i tant que str[i] n'est pas une lettre
 			while( str[i] == '\0' || str[i] == ' ' || str[i] == '\t')
 				i--;
 
-			//On sauvegarde la position de i car str[i] est forcement la derniere lettre d'un mot
+			// 4. On sauvegarde la position de i car str[i] est forcement la derniere lettre d'un mot
 			end = i;
 			
-			//On recule i tant que str[i] n'est pas un separateur (une lettre)
+			// 5. On recule i tant que str[i] n'est pas un separateur (une lettre)
 			while(str[i] && str[i] != ' ' && str[i] != '\t')
 				i--;
 
-			//i est sur le caractere avant la 1ere lettre d'un mot (le meme que end)
-			//Donc on sauvegarde la position de cette lettre dans start (i + 1)
+			// 6. On sauvegarde la position de i+1 (premierre lettre) car i est sur l'espace avant cette lettre (d'ou le +1)
 			start = i + 1;
 
-			//On met flag a 1 pour write un espace apres avoir ecrit le mot
-			flag = 1;
+			// 7. On met flg a 1 pour write un espace apres avoir ecrit le mot
+			flg = 1;
 
-			//On write le mot de start a end.
+			// 8. On write le mot de start a end.
 			while(start <= end)
 			{
 				write (1, &str[start],1);
 				start++;		
 			}
 
-			//On write un espace si flag est egal a 1
-			if (flag != 0)
+			// 9. On write un espace si flg est egal a 1
+			if (flg != 0)
 			{
 				write(1, " ", 1);
 			}
@@ -54,3 +55,5 @@ int main(int ac, char **av)
 	}
 	write(1, "\n", 1);
 }
+
+//flg est la pour eviter qu'on mette un espace de trop
