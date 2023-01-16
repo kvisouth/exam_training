2bb8c0754d6570bb04b19a9df54265f44919e7ff
nokosse <visouthivong.kevin@gmail.com>
2023-01-16 16:00:11 +0100

    added comment

--- LEVEL_3/fprime.c
+++ LEVEL_3/fprime.c
@@ -26,12 +26,20 @@ int		main(int ac, char **av)
             int prime = 2;
             while (nb > 1)
             {
+                //Si prime est un diviseur de nb.
                 if (nb % prime == 0)
                 {
+                    // 1. Print le diviseur (prime)
                     printf("%d", prime);
+
+                    // 2. Divise nb par prime.
                     nb /= prime;
-                    if (nb > 1) // Pour pas mettre de '*' a la fin.
+
+                    // 3. Print un '*' si nb > 1
+                    if (nb > 1)
                         printf("*");
+
+                    // 4. Decremente prime de 1.
                     prime--;
                 }
                 prime++;
