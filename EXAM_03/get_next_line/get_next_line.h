aa2c864a7cd6bea7eb58b25925fc0eefb208c03e
nokosse <visouthivong.kevin@gmail.com>
2023-02-04 19:14:05 +0100

    added get_next_line rank 03

---
+++ EXAM_03/get_next_line/get_next_line.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kvisouth <kvisouth@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/12 10:43:17 by kvisouth          #+#    #+#             */
+/*   Updated: 2023/01/19 00:35:26 by kvisouth         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>  // for malloc
+# include <unistd.h>  // for read
+# include <stddef.h>  // for size_t
+# include <stdio.h>   // for printf in main for testing
+# include <fcntl.h>   // for open in main for testing
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5
+# endif
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(const char *str);
+char	*ft_strjoin(const char *s1, const char *s2);
+char	*ft_substr(const char *s, unsigned int start, size_t len);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(const char *s1);
+
+#endif
