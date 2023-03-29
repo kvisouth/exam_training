8ae84fcb0682291a5cd647f7da7da84be418fdfe
nokosse <visouthivong.kevin@gmail.com>
2023-03-29 18:29:54 +0200

    simplified

--- EXAM_03/ft_printf/ft_printf.c
+++ EXAM_03/ft_printf/ft_printf.c
@@ -1,144 +1,114 @@
-#include <stdarg.h>
 #include <unistd.h>
+#include <stdarg.h>
 #include <stdio.h>
 
-// Il faut faire 6 fonctions pour printf.
-// - ft_putstr : affiche la chaine de caractère passée en paramètre.
-//			Elle renvoie le nombre de caractères affichés (i)
-//
-// - ft_putchar : affiche le caractère passé en paramètre.
-//			Elle renvoie 1 (car un seul caractère a été affiché)
-//
-// - ft_putnbr : affiche l’entier passé en paramètre. (int)
-//			Elle renvoie le nombre de caractères affichés (count)
-//
-// - ft_puthex : affiche l’entier passé en paramètre en hexadécimal.
-//			Elle renvoie le nombre de caractères affichés (count)
-//
-// - ft_putptr : Affiche 0x suivi d'un hexadécimal. (celui en paramètre)
-//			Elle renvoie le nombre de caractères affichés (count)
-//
-// - ft_convert : Va permettre a printf de choisir la bonne fonction parmis
-//			les 5 précédentes. (ft_putstr, ft_putchar, ft_putnbr, ft_puthex, ft_putptr)
-//			Selon la lettre passée en paramètre. (cspdiuxX%).
-//			Elle renvoie le nombre de caractères affichés (count)
+int ft_putchar(char c)
+{
+    write(1,&c,1);
+    return 1;
+}
 
-int	ft_putstr(char *str)
+int ft_putstr(char *str)
 {
     int i = 0;
-	if (str)
-	{
-		while (str[i])
-			write (1, &str[i++], 1);
-		return (i);
-	}
-	return ft_putstr("(null)");
+    if (str)
+    {
+        while (str[i])
+            write(1,&str[i++],1);
+        return i;
+    }
+    return (ft_putstr("(null)"));
 }
 
-int	ft_putchar(char c)
-{
-	write(1, &c, 1);
-	return (1);
-}
-
-int	ft_putnbr(long int nb)
+int ft_putnbr(long int nb)
 {
     int count = 0;
-	if (nb < 0)
-	{
-		count += ft_putchar('-');
-		nb *= -1;
-	}
-	if (nb >= 10)
-	{
-		count += ft_putnbr(nb / 10);
-		count += ft_putnbr(nb % 10);
-	}
-	else
-		count += ft_putchar(nb + 48);
-	return count;
+    if (nb < 0)
+    {
+        count += ft_putchar('-');
+        nb *= -1;
+    }
+    if (nb >= 10)
+    {
+        count += ft_putnbr(nb/10);
+        count += ft_putnbr(nb%10);
+    }
+    else
+        count += ft_putchar(nb + 48);
+    return count;
 }
 
-int	ft_puthex(unsigned long nb, char caps)
+int ft_puthex(unsigned long nb, char caps)
 {
-	int	count = 0;
-	if (nb >= 16)
-	{
-		count += ft_puthex((nb / 16), caps);
-		count += ft_puthex((nb % 16), caps);
-	}
-	else
-	{
-		if (nb < 10)
-			count += ft_putnbr(nb);
-		else
-			count += ft_putchar(nb - 10 + 'a' + caps - 'x');
-	}
-	return count;
+    int count = 0;
+    if (nb >= 16)
+    {
+        count += ft_puthex(nb/16, caps);
+        count += ft_puthex(nb%16, caps);
+    }
+    else
+    {
+        if (nb < 10)
+            count += ft_putnbr(nb);
+        else
+            count += ft_putchar(nb - 10 + 'a' + caps - 'x');
+    }
+    return count;
 }
 
-int	ft_putptr(unsigned long ptr)
+int ft_putptr(unsigned long ptr)
 {
-	int	count = 0;
-	if (!ptr)
-		return (ft_putstr("(nil)"));
-	count += ft_putstr("0x");
-	count += ft_puthex(ptr, 'x');
-	return count;
+    int count = 0;
+    if (!ptr)
+        return (ft_putstr("(nil)"));
+    count += ft_putstr("0x");
+    count += ft_puthex(ptr, 'x');
+    return count;
 }
 
-// cspdiuxX%
-// c : int
-// s : char *
-// p : unsigned long
-// d : int
-// i : int
-// u : unsigned int
-// x : unsigned int
-// X : unsigned int
-int	ft_convert(const char letter, va_list args)
+//cspdiuxX%
+int convert(const char c, va_list arg)
 {
-	int	count = 0;
-
-	if (letter == 'c')
-		count += ft_putchar(va_arg(args, int));
-	else if (letter == 's')
-		count += ft_putstr(va_arg(args, char *));
-	else if (letter == 'p')
-		count += ft_putptr(va_arg(args, unsigned long));
-	else if (letter == 'd')
-		count += ft_putnbr(va_arg(args, int));
-	else if (letter == 'i')
-		count += ft_putnbr(va_arg(args, int));
-	else if (letter == 'u')
-		count += ft_putnbr(va_arg(args, unsigned int));
-	else if (letter == 'x')
-		count += ft_puthex(va_arg(args, unsigned int), 'x');
-	else if (letter == 'X')
-		count += ft_puthex(va_arg(args, unsigned int), 'X');
-	else if (letter == '%')
-		count += ft_putchar(letter);
-	return count;
+    int count = 0;
+    if (c == 'c')
+        count += ft_putchar(va_arg(arg, int));
+    else if (c == 's')
+        count += ft_putstr(va_arg(arg, char *));
+    else if (c == 'p')
+        count += ft_putptr(va_arg(arg, unsigned long));
+    else if (c == 'd')
+        count += ft_putnbr(va_arg(arg, int));
+    else if (c == 'i')
+        count += ft_putnbr(va_arg(arg, int));
+    else if (c == 'u')
+        count += ft_putnbr(va_arg(arg, unsigned int));
+    else if (c == 'x')
+        count += ft_puthex(va_arg(arg, unsigned int), 'x');
+    else if (c == 'X')
+        count += ft_puthex(va_arg(arg, unsigned int), 'X');
+    else if (c == '%')
+        count += ft_putchar(c);
+    return count;
 }
 
 int	ft_printf(const char *format, ...)
 {
-	int			i = 0;
-	int			count = 0;
-	va_list		args;
+    int i = 0;
+    int count = 0;
+    va_list arg;
 
-	va_start (args, format);
+	va_start (arg, format);
 	while (format[i])
 	{
 		if (format[i] == '%')
 		{
-			count += ft_convert(format[i + 1], args);
+			count += convert(format[i + 1], arg);
 			i++;
 		}
 		else
 			count += ft_putchar(format[i]);
 		i++;
 	}
-	va_end(args);
+	va_end(arg);
 	return count;
 }
